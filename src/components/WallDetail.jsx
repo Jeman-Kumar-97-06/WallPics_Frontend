eb229df11807728d079a3a28ded29a01fd6fc7af
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-25 16:24:20 +0530

    Removed cached files

--- src/components/WallDetail.jsx
+++ src/components/WallDetail.jsx
@@ -1,36 +1,51 @@
-const WallDetail = ({w}) => {
+const WallDetail = ({wallpaper}) => {
 
-    let fullUrl = `https://my-projects-lxja.onrender.com/${w.wall}`
+    let fullUrl = `https://my-projects-lxja.onrender.com/${wallpaper.wall}`
 
     return (
-        <div class="max-w-sm mr-12 mt-6 h-auto min-h-3 p-6 bg-gray-100 rounded-lg shadow-lg">
-        <div class="relative">
-          <img 
-            className="wallpaper-img"
-            src={fullUrl} 
-            alt="Wallpaper" 
-            class="w-full h-auto rounded-lg"
-          />
-    
-          <a 
+
+      <div key={wallpaper.id} className="mb-4 relative mr-5">
+        <img
+          src={fullUrl}
+          alt="Some Wallpaper"
+          className="w-full h-auto rounded-lg"
+        />
+        <a 
             href={fullUrl} 
             download
-            class="absolute bottom-4 right-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition"
+            className="absolute bottom-4 right-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition"
           >
             Download
           </a>
-        </div>
-        {/* <div class="mt-4 text-gray-800">
-          <h2 class="text-2xl font-bold">Beautiful Nature Wallpaper</h2>
-          <p class="mt-2 text-sm">
-            A stunning high-resolution nature wallpaper to enhance your device background.  
-          </p>
-          <div class="mt-4 flex space-x-4 text-sm">
-            <span class="bg-gray-200 px-3 py-1 rounded-lg">Resolution: 1920x1080</span>
-            <span class="bg-gray-200 px-3 py-1 rounded-lg">Category: Nature</span>
-          </div>
-        </div> */}
       </div>
+      //   <div class="max-w-sm mr-12 mt-6 h-auto min-h-3 p-6 bg-gray-100 rounded-lg shadow-lg">
+      //   <div class="relative">
+      //     <img 
+      //       className="wallpaper-img"
+      //       src={fullUrl} 
+      //       alt="Wallpaper" 
+      //       class="w-full h-auto rounded-lg"
+      //     />
+    
+      //     <a 
+      //       href={fullUrl} 
+      //       download
+      //       class="absolute bottom-4 right-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition"
+      //     >
+      //       Download
+      //     </a>
+      //   </div>
+      //   {/* <div class="mt-4 text-gray-800">
+      //     <h2 class="text-2xl font-bold">Beautiful Nature Wallpaper</h2>
+      //     <p class="mt-2 text-sm">
+      //       A stunning high-resolution nature wallpaper to enhance your device background.  
+      //     </p>
+      //     <div class="mt-4 flex space-x-4 text-sm">
+      //       <span class="bg-gray-200 px-3 py-1 rounded-lg">Resolution: 1920x1080</span>
+      //       <span class="bg-gray-200 px-3 py-1 rounded-lg">Category: Nature</span>
+      //     </div>
+      //   </div> */}
+      // </div>
     )
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,17 @@ import WallDetail from "../components/WallDetail";
 import { useEffect } from "react";
 import {useWallContext} from '../hooks/useWallContext';
 import {useAuthContext} from '../hooks/useAuthContext';
+import Masonry from "react-masonry-css";
 
 const Home = () => {
     const {walls,dispatch} = useWallContext();
     const {user}     = useAuthContext();
+    const breakpointColumns = {
+      default: 5,
+      1100: 4,
+      768: 3,
+      500: 2
+    };
     useEffect(()=>{
         const fetchAllWalls = async () => {
           const resp  = await fetch('https://my-projects-lxja.onrender.com/api/walls/',{headers:{"Authorization":`Bearer ${user.token}`}});
@@ -23,11 +30,17 @@ const Home = () => {
 
     return (
         <div className='w-screen mt-5 ml-5 flex flex-wrap'>
+        <Masonry
+          breakpointCols={breakpointColumns}
+          className="flex"
+          columnClassName="masonry-grid_column"
+        >
           {walls && walls.map(
             w=>(
-              <WallDetail key={w._id} w={w}/>
+              <WallDetail key={w._id} wallpaper={w}/>
             )
           )}
+        </Masonry>
         </div>
     )
 };
